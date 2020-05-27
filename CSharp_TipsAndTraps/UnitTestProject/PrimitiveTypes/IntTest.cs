700efbef958a1566ce8b05112a46e42904ed5753
Glaucio Oliveira <glaucio.dev@gmail.com>
2020-05-27 09:59:22 -0300

    adding new tests

--- CSharp_TipsAndTraps/UnitTestProject/PrimitiveTypes/IntTest.cs
+++ CSharp_TipsAndTraps/UnitTestProject/PrimitiveTypes/IntTest.cs
@@ -2,11 +2,65 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.Diagnostics;
 
 namespace UnitTestProject.PrimitiveTypes
 {
     [TestClass]
     public class IntTest
     {
+        [TestMethod]
+        public void ThreePartFormat()
+        {
+            var format = "Boa Avaliação #;Avaliação Ruim -#;Novo Item";
+
+            var avaliacoes = new int[] {-10, 5, 0, 15 };
+
+            foreach (var avaliacao in avaliacoes)
+                Debug.WriteLine($"Nota: {avaliacao} Avaliação: {avaliacao.ToString(format)}");
+        }
+
+        [TestMethod]
+        public void CustomFormatUsage()
+        {
+            var avaliacoes = new int[] { -10, 5, 0, 15 };
+
+            foreach (var avaliacao in avaliacoes)
+                Debug.WriteLine(string.Format(new ProdutividadeFormatProvider(),"{0}", avaliacao));
+        }
+
+        [TestMethod]
+        public void StringToInt()
+        {
+            var avaliacoes = new int[] { -10, 5, 0, 15 };
+
+            foreach (var avaliacao in avaliacoes)
+                Debug.WriteLine(string.Format(new ProdutividadeFormatProvider(),"{0}", avaliacao));
+        }
     }
+
+    class ProdutividadeFormatProvider : IFormatProvider, ICustomFormatter
+    {
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            int rating = (int)arg;
+
+            if (rating > 10)
+                return $"Nota: {rating}, Excelente!!";
+            else if (rating > 4)
+                return $"Nota: {rating}, Bom Trabalho.";
+            else if (rating > 1)
+                return $"Nota: {rating}. Continue tentando!";
+            else
+                return $"Nota: {rating}. Procure ajuda.";
+        }
+
+        public object GetFormat(Type formatType)
+        {
+            return this;
+        }
+    }
+
+
 }
--- CSharp_TipsAndTraps/UnitTestProject/PrimitiveTypes/StringTest.cs
+++ CSharp_TipsAndTraps/UnitTestProject/PrimitiveTypes/StringTest.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
 using System.Text;
 
 namespace UnitTestProject.PrimitiveTypes
@@ -10,10 +13,64 @@ namespace UnitTestProject.PrimitiveTypes
     {
 
         [TestMethod]
-        public void CheckIfStringIsEmpty()
+        public void JoinArray()
+        {
+            var profissoes = new string[] { "Dentista", "Desenvolvedor", "Motorista" };
+
+            Debug.Print(string.Join(", ", profissoes));
+        }
+
+        [TestMethod]
+        public void StringBuilder()
+        {
+            StringBuilder sb = new StringBuilder();
+            var profissoes = new string[] { "Dentista", "Desenvolvedor", "Motorista" };
+
+            foreach (var profissao in profissoes) sb.Append(profissao);
+
+            Debug.Print(sb.ToString() + " ");
+        }
+
+
+        [TestMethod]
+        public void IsEmpty()
         {
             var input = "   ";
             var condition = string.IsNullOrWhiteSpace(input) == true;
+
+            Assert.IsTrue(condition);
+        }
+
+
+
+        [TestMethod]
+        public void StringInterpolation()
+        {
+            var nome = "Gláucio Oliveira";
+            var profissao = "Desenvolvedor";
+
+            var input = $"Olá, meu nome é {nome}";
+
+            var inputAlignedLeft = $"Nome: {nome,-50} Profissão: {profissao,-50}";
+            var inputAlignedRight = $"Nome: {nome,50} Profissão: {profissao,50}";
+
+            Debug.WriteLine(inputAlignedLeft);
+            Debug.WriteLine(inputAlignedRight);
+
+            var condition = input == "Olá, meu nome é Gláucio Oliveira";
+
+            Assert.IsTrue(condition);
+        }
+
+        [TestMethod]
+        public void IsValidUnicode()
+        {
+            var input = "º";
+            char inputCode = (char)32;// input.First();
+
+            UnicodeCategory ucCategory = char.GetUnicodeCategory(inputCode);
+
+            var condition = ucCategory != UnicodeCategory.OtherNotAssigned;
 
             Assert.IsTrue(condition);
         }
