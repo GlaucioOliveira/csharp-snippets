94f38f5eb320635f84230030a71dea9288ffae71
Glaucio Oliveira <glaucio.dev@gmail.com>
2020-05-26 21:52:07 -0300

    adding initial solution

---
+++ CSharp_TipsAndTraps/UnitTestProject/PrimitiveTypes/IntTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestProject.PrimitiveTypes
+{
+    [TestClass]
+    public class IntTest
+    {
+    }
+}
---
+++ CSharp_TipsAndTraps/UnitTestProject/PrimitiveTypes/StringTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestProject.PrimitiveTypes
+{
+    [TestClass]
+    public class StringTest
+    {
+
+        [TestMethod]
+        public void CheckIfStringIsEmpty()
+        {
+            var input = "   ";
+            var condition = string.IsNullOrWhiteSpace(input) == true;
+
+            Assert.IsTrue(condition);
+        }
+    }
+}
